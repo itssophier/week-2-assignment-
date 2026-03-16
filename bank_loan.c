97da3e2e1ea9abfa52196af91b49146cbb9ff07d
Sofia Juma Mohamed <144211327+itssophier@users.noreply.github.com>
2026-03-16 09:24:45 +0300

    Add comments for clarity in loan eligibility check
    Added comments for user input and eligibility check.

--- bank_loan.c
+++ bank_loan.c
@@ -3,13 +3,13 @@
 int main() {
     int age;
     float income;
-
+//take user input for age
     printf("Enter your age: ");
     scanf("%d", &age);
-
+//take user input for annual income
     printf("Enter your annual income (Sh): ");
     scanf("%f", &income);
-
+//check for elegibility to qualify the loan
     if (age >= 21 && income >= 21000) {
         printf("Congratulations you qualify for a loan.\n");
     }
